1d22318b924973d6d3eb8b0fe9906e40601bddb4
tracer-ics2020 <tracer@njuics.org>
2020-10-04 22:40:29 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: e32b0e539dea47aaba38836ea4633746   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  22:40:29 up 13:03,  1 user,  load average: 0.59, 0.71, 0.82

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,10 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+static int cmd_si(char *args);
+//static int cmd_info(char *args);
+//static int cmd_x(char *args);
+
 
 static struct {
   char *name;
@@ -47,7 +51,9 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  {"si", "Step one instruction exactly", cmd_si },
+//  {"info", "Show things about the program being debugged", cmd_info },
+//  {"x", "Scan memory", cmd_x }
   /* TODO: Add more commands */
 
 };
@@ -74,6 +80,23 @@ static int cmd_help(char *args) {
     }
     printf("Unknown command '%s'\n", arg);
   }
+  return 0;
+}
+
+static int cmd_si(char *args){
+  char *arg = strtok(NULL, " ");
+  int num = 0;
+
+  if(arg == NULL){
+    cpu_exec(1);
+	return 0;
+  }
+  sscanf(arg, "%d" , &num);
+  if(num < 0){
+    printf("Please input a legal number\n");
+    return 0;
+  }
+  cpu_exec(num);
   return 0;
 }
 
