4e703a5c29191c99ea4fba01e4bcb7b3b352701f
tracer-ics2020 <tracer@njuics.org>
2020-10-26 19:14:56 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: a39e5091b8614bde83e1a243828546d0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  19:14:56 up 48 min,  1 user,  load average: 1.03, 1.19, 1.17

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -58,7 +58,7 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[65536] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
@@ -81,7 +81,7 @@ static bool make_token(char *e) {
         position += substr_len;
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
+	         * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
 
