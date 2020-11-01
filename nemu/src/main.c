caa19decb249a2b1e98f2f0d6b1b03b46d062ec9
tracer-ics2020 <tracer@njuics.org>
2020-11-01 15:25:09 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: d5fd374557264e46a293182d35db6c22   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  15:25:09 up  5:51,  1 user,  load average: 1.29, 1.15, 1.04

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+#include "monitor/debug/expr.h"
+
 void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
@@ -5,7 +10,24 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
-
+  char e[1000];
+  FILE* fp = fopen("../tools/gen-expr/input","r");
+  assert(fp!=NULL);
+  int ans=0;
+  while(fscanf(fp,"%d",&ans)==1){
+    if(ans == -1) continue;
+    if(fscanf(fp,"%s",e)==EOF) continue;
+    bool success =true;
+    int my_ans = expr(e,&success);
+    if(success == false){
+      printf("not a legal expression!\n");
+      continue;
+    }
+    if(my_ans != ans){
+      printf("Wrong answer:my answer is %d, but the true answer is %d\n",my_ans,ans);
+      printf("the test is %s\n",e);
+    }
+  }
   /* Start engine. */
   engine_start();
 
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -15,10 +15,50 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
+static int buf_l = 0; //buf_length
+
+static inline uint32_t choose(uint32_t t){
+  return rand() % t;
+}
+
+static inline void gen(char c){
+  if(buf_l > 655) return;
+  buf[buf_l++] = c;
+  return;
+}
+
+static inline void gen_num(){
+  if(buf_l > 655) return;
+  else{
+    int i = rand() % 2000;
+    if(i == 0) i++;
+    int tmp = 1,cnt=0;
+    while(i>=tmp){
+      tmp*=10;
+      cnt++;
+    }
+    sprintf(buf+buf_l,"%d",i);
+    buf_l+=cnt;
+  }  
+}
+
+
+static inline void gen_rand_op(){
+  switch(choose(4)){
+    case 0: gen('+'); break;
+    case 1: gen('-'); break;
+    case 2: gen('*'); break;
+    case 3: gen('/'); break;
+    default: assert(0); break;
+  }
+}
 
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
-
+  switch(choose(3)){
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }  
 }
 
 int main(int argc, char *argv[]) {
@@ -30,7 +70,13 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    buf_l = 0;
+    memset(buf,'\0',sizeof(buf));
     gen_rand_expr();
+    if(buf_l > 655){
+      i--;
+      continue;
+    }
 
     sprintf(code_buf, code_format, buf);
 
@@ -46,7 +92,8 @@ int main(int argc, char *argv[]) {
     assert(fp != NULL);
 
     int result;
-    fscanf(fp, "%d", &result);
+    if(fscanf(fp, "%d", &result)==EOF) 
+    printf("-1\n");
     pclose(fp);
 
     printf("%u %s\n", result, buf);
