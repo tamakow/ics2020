4ca22222370ce00127b1c77b5aca4b767bcc6e7c
tracer-ics2020 <tracer@njuics.org>
2020-12-03 12:16:56 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 664d92d422e3485c998d9bc1cdc9ac52   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  12:16:56 up  3:33,  1 user,  load average: 0.73, 0.90, 1.04

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -277,7 +277,7 @@ uint32_t eval(int p,int q){
 			case TK_EQ:return val1==val2;
 			case TK_NEQ:return val1!=val2;
 			case TK_AND:return val1&&val2;
-			case TK_NEG:assert(0);return 0-val2;
+			case TK_NEG:return 0-val2;
 		  	default: assert(0);
 	  	}
 	}
