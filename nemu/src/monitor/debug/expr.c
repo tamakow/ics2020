5d479adc6bf2e58306044f1fc0caf7a9fa3e5801
tracer-ics2020 <tracer@njuics.org>
2020-12-10 11:34:57 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: a47a0d5853e24f60975a5e73da3a875f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  11:34:57 up  1:43,  1 user,  load average: 0.74, 0.42, 0.40

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -197,7 +197,7 @@ int judge_operator(int i){
 		return 3;   
 	if(tokens[i].type == '*' || tokens[i].type == '/' )
 		return 4;
-	if(tokens[i].type == TK_NEG) return 6;
+	//if(tokens[i].type == TK_NEG) return 6;
 	else return 1000;
 }
 
@@ -264,8 +264,7 @@ uint32_t eval(int p,int q){
 	  	//if(tokens[p].type==TK_NEG) return 0-eval(p+1,q);
 		if(tokens[p].type==TK_POINT)   return vaddr_read(eval(p+1,q),4); // this is not true if the following expression is not TK_NUM or '('+ expressoin+')'
 	  int op = find_main_operator(p,q);//TODO to find the main
-		if(op==-1) assert(0);
-    if(tokens[op].type==TK_NEG) return 0-eval(op+1,q);
+    if(op==-1&&tokens[p].type==TK_NEG) return 0-eval(op+1,q);
 	  uint32_t val1 = eval(p , op - 1);
 	  uint32_t val2 = eval(op + 1, q);
 
