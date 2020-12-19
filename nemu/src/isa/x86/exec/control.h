083c98ea497d6329f3a94a85381db3a178d9f537
tracer-ics2020 <tracer@njuics.org>
2020-12-19 10:04:51 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 62ff24cbefcd4a95983d01cee250f583   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  10:04:51 up 15:47,  1 user,  load average: 1.42, 1.37, 1.29

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -24,14 +24,16 @@ static inline def_EHelper(jmp_rm) {
 
 static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
-
+  //TODO();
+  rtl_push(s,&s->seq_pc);
+  rtl_j(s,s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
 }
 
 static inline def_EHelper(ret) {
-  TODO();
-
+  //TODO();
+  rtl_pop(s,&s->jmp_pc);
+  rtl_j(s,s->jmp_pc);
   print_asm("ret");
 }
 
--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -111,6 +111,9 @@ again:
     IDEXW(0xd2, gp2_cl2E, gp2, 1)
     IDEX (0xd3, gp2_cl2E, gp2)
     EX   (0xd6, nemu_trap)
+    //start
+    IDEX (0xe8, J, call)
+    //done
     IDEXW(0xf6, E, gp3, 1)
     IDEX (0xf7, E, gp3)
     IDEXW(0xfe, E, gp4, 1)
