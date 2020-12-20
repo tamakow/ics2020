07e257df34321c36f0f3744dea935ce5c07bf275
tracer-ics2020 <tracer@njuics.org>
2020-12-20 10:07:56 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 256db0761a3c4f9d85766eb3e6f7d027   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  10:07:56 up 46 min,  1 user,  load average: 0.52, 0.45, 0.49

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -17,14 +17,15 @@ static inline void cmp_internal(DecodeExecState *s) {
  
 
 static inline def_EHelper(sub) {
-  //TODO();
-  rtl_sub(s,s0,ddest,dsrc1);
-  operand_write(s,id_dest,s0);
-  // rtl_update_ZFSF(s,s0,id_dest->width);
-  // rtl_is_sub_carry(s,s1,ddest,dsrc1);//problem
-  // rtl_set_CF(s,s1);
-  // rtl_is_sub_overflow(s,s1,s0,ddest,dsrc1,id_dest->width);
-  // rtl_set_OF(s,s1);
+  rtl_sub(s, s1, ddest, dsrc1);      //s1<-dest-src1-
+  rtl_update_ZFSF(s, s1, id_dest->width);
+  rtl_is_sub_overflow(s, s2, s1, ddest, dsrc1, id_dest->width);
+  rtl_set_OF(s, s2);
+  // rtl_is_add_carry(s, s2, s0, dsrc1);
+  rtl_is_sub_carry(s, s0, ddest, dsrc1);
+  // rtl_or(s, s0, s0, s2);
+  rtl_set_CF(s, s0);
+  operand_write(s, id_dest, s1);
   print_asm_template2(sub);
 }
 
