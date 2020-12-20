bb3d2e083fa99c7fd5b10c0ca26b1e0d083d856e
tracer-ics2020 <tracer@njuics.org>
2020-12-20 09:57:54 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 256db0761a3c4f9d85766eb3e6f7d027   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  09:57:54 up 36 min,  1 user,  load average: 0.51, 0.65, 0.54

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -103,7 +103,7 @@ again:
     IDEXW(0x80, I2E, gp1, 1)
     IDEX (0x81, I2E, gp1)
     IDEX (0x83, SI2E, gp1)
-    EX   (0x85, test)       //jkjkjk
+    //EX   (0x85, test)       //jkjkjk
     IDEXW(0x88, mov_G2E, mov, 1)
     IDEX (0x89, mov_G2E, mov)
     IDEXW(0x8a, mov_E2G, mov, 1)
