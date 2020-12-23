82c9fa290ec15b8ad2ecb840e5cdd2b1000cbbe1
tracer-ics2020 <tracer@njuics.org>
2020-12-23 21:32:38 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: f3c169a69cd24a1f9b1c6dcb46246b66   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  21:32:38 up  7:22,  1 user,  load average: 1.09, 1.05, 1.19

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -27,7 +27,7 @@ static inline def_EHelper(xor) {
   rtl_li(s,s1,0);
   rtl_set_OF(s,s1);
   rtl_set_CF(s,s1);
-  rtl_update_ZFSF(s,ddest,id_dest->width);
+  rtl_update_ZFSF(s,s0,id_dest->width);
   print_asm_template2(xor);
 }
 
@@ -38,28 +38,35 @@ static inline def_EHelper(or) {
   rtl_li(s,s1,0);
   rtl_set_OF(s,s1);
   rtl_set_CF(s,s1);
-  rtl_update_ZFSF(s,ddest,id_dest->width);
+  rtl_update_ZFSF(s,s0,id_dest->width);
   print_asm_template2(or);
 }
 
 static inline def_EHelper(sar) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
-
+  rtl_sext(s,ddest,ddest,id_dest->width);
+  rtl_sar(s,s0,ddest,dsrc1);
+  operand_write(s,id_dest,s0);
+  rtl_update_ZFSF(s,s0,id_dest->width);
   print_asm_template2(sar);
 }
 
 static inline def_EHelper(shl) {
   TODO();
   // unnecessary to update CF and OF in NEMU
-
+  rtl_shl(s,s0,ddest,dsrc1);
+  operand_write(s,id_dest,s0);
+  rtl_update_ZFSF(s,s0,id_dest->width);
   print_asm_template2(shl);
 }
 
 static inline def_EHelper(shr) {
   TODO();
   // unnecessary to update CF and OF in NEMU
-
+  rtl_shr(s,s0,ddest,dsrc1);
+  operand_write(s,id_dest,s0);
+  rtl_update_ZFSF(s,s0,id_dest->width);
   print_asm_template2(shr);
 }
 
