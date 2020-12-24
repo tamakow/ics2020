f2e6df1f5740d835c6152b9222bf50c595495dd7
tracer-ics2020 <tracer@njuics.org>
2020-12-24 10:54:46 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 9f600687e1244462a373eb80a4315734   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  10:54:46 up  2:34,  1 user,  load average: 7.18, 6.92, 6.06

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -14,7 +14,7 @@ static inline void set_width(DecodeExecState *s, int width) {
 static inline def_EHelper(gp1) {
   switch (s->isa.ext_opcode) {
     EX(0, add) EX(1, or) EX(2, adc) EMPTY(3)
-    EX(4, and) EXW(5, sub,-1) EX(6, xor) EX(7, cmp)
+    EX(4, and) EX(5, sub) EX(6, xor) EX(7, cmp)
   }
 }
 
