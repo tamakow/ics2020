a0ca330b9a5d751d7cda2c083b338c11460d7d79
tracer-ics2020 <tracer@njuics.org>
2020-12-26 23:32:07 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 2e21668f5f514f059f98ad5f3ef8e1ca   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  23:32:07 up 11 min,  1 user,  load average: 0.72, 0.81, 0.60

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,14 +5,6 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
-}
-
 char* zx_itoa(int num,char* str,int radix){ 
   if(radix < 2 || (num<0 && radix != 10)){ //set radix 10 normally
     return NULL;
@@ -48,6 +40,48 @@ char* zx_itoa(int num,char* str,int radix){
   }
   return str;
 }
+
+int printf(const char *fmt, ...) {
+  int len = strlen(fmt);
+  int pre=0;
+  int return_val=0;
+  va_list Args;
+  va_start(Args, fmt);
+  while(pre<len){
+    if(fmt[pre] != '%'){
+      putch(fmt[pre++]);
+      return_val++;
+    }
+    else{
+      pre++;
+      if(fmt[pre]== 's'){
+        char *tmp = va_arg(Args,char*);
+        for(int i=0;i<strlen(tmp);++i) putch(tmp[i]);
+        return_val += strlen(tmp);
+      }
+      else if(fmt[pre] == 'd'){
+        int tmp_num = va_arg(Args,int);
+        char tmp[128]="";
+        zx_itoa(tmp_num,tmp,10);
+        for(int i=0;i<strlen(tmp);++i) putch(tmp[i]);
+        return_val += strlen(tmp);
+      }
+      else {
+        printf("to be continue\n");
+        assert(0);
+      }
+      pre++;
+    }
+  }
+  va_end(Args);
+  return return_val;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  return 0;
+}
+
+
 
 int sprintf(char *out, const char *fmt, ...) {
   int len = strlen(fmt);
