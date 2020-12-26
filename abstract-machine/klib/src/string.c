a1b4ceb4470b2f202c40ba1ea0669a3be2ebccec
tracer-ics2020 <tracer@njuics.org>
2020-12-26 09:35:39 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0f5a19dfd0454e01b4697954df3ceaf5   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  09:35:40 up  8:51,  1 user,  load average: 1.39, 1.60, 1.39

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -3,31 +3,59 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  return 0;
+size_t strlen(const char *s) { 
+  size_t len = 0;
+  while(*(s+len)!='\0'){
+    ++len;
+  }
+  return len;
 }
 
-char *strcpy(char* dst,const char* src) {
-  return NULL;
+char *strcpy(char* dst,const char* src) {  
+  char *tmp = dst;
+  while(*src!='\0'){
+    *tmp++ = *src++;
+  }
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char *tmp = dst;
+  while(n>0&&*src!='\0'){
+    *tmp++= *src++;
+    n--;
+  }
+  while(n--){
+    *tmp++='\0';
+  }
+  return dst;
 }
 
-char* strcat(char* dst, const char* src) {
-  return NULL;
+char* strcat(char* dst, const char* src) { 
+  char *tmp= dst;
+  while(*tmp!='\0') tmp++;
+  while(*src!='\0'){
+    *tmp++=*src++;
+  }
+  *tmp='\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  while(*s1!='\0'&&*s2!='\0'&&*s1==*s2){
+    s1++;
+    s2++;
+  }
+  if(*s1 == *s2) return 0;
+  else if(*s1 > *s2) return 1;
+  else return -1;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
   return 0;
 }
 
-void* memset(void* v,int c,size_t n) {
+void* memset(void* v,int c,size_t n) { //TODO
   return NULL;
 }
 
@@ -35,11 +63,11 @@ void* memmove(void* dst,const void* src,size_t n) {
   return NULL;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
+void* memcpy(void* out, const void* in, size_t n) { //TODO
   return NULL;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n) {
+int memcmp(const void* s1, const void* s2, size_t n) { //TODO
   return 0;
 }
 
