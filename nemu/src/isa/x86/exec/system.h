9c2fc0675443ea12759cc746f8417fdff7e3d1ec
tracer-ics2020 <tracer@njuics.org>
2020-12-26 15:32:18 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0f5a19dfd0454e01b4697954df3ceaf5   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  15:32:18 up 14:47,  1 user,  load average: 3.32, 3.08, 2.88

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -66,9 +66,9 @@ static inline def_EHelper(in) {
 static inline def_EHelper(out) {
   //TODO();
   switch(id_dest->width){
-    case 4: pio_write_l(*ddest,*dsrc1); break;
-    case 2: pio_write_w(*ddest,*dsrc1); break;
-    case 1: pio_write_b(*ddest,*dsrc1); break;
+    case 4: pio_write_l(id_dest->val,id_src1->val); break;
+    case 2: pio_write_w(id_dest->val,id_src1->val); break;
+    case 1: pio_write_b(id_dest->val,id_src1->val); break;
     default: assert(0);
   }
   print_asm_template2(out);
