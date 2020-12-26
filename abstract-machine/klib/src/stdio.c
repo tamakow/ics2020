1d5e7a634452c0aa86d5c43af53bd0ca9a94e51a
tracer-ics2020 <tracer@njuics.org>
2020-12-26 11:17:14 +0800

    >  run 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0f5a19dfd0454e01b4697954df3ceaf5   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:17:14 up 10:32,  1 user,  load average: 1.24, 1.33, 1.55

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,81 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 }
 
+char* zx_itoa(int num,char* str,int radix){ 
+  if(radix < 2 || (num<0 && radix != 10)){ //set radix 10 normally
+    return NULL;
+  }
+  if(num == 0){
+    *str = '0';
+    *(str+1) = '\0';
+    return str;
+  }
+
+  char *p =str;
+  int src=num;
+  num = (src<0)?num*(-1):num;
+  while(num!=0){
+    if(num%radix <10){
+      *p = num%radix + '0';
+    }
+    else{
+      *p = num%radix + 'a';
+    }
+    num = num/radix;
+    p++;
+  }
+  if(src<0){
+    *p = '-';
+    p++;
+  }
+  *p='\0';
+  for(char* left =str,*right = p-1;left<right;left++,right--){
+    char temp = *left;
+    *left = *right;
+    *right =temp;
+  }
+  return str;
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  int len = strlen(fmt);
+  int pre=0;
+  int return_val=0;
+  *out = '\0';
+  va_list Args;
+  va_start(Args, fmt);
+  while(pre<len){
+    if(fmt[pre] != '%'){
+      char tmp[2];
+      tmp[0]=fmt[pre];
+      tmp[1]='\0';
+      strcat(out,tmp);
+      pre++;
+      return_val++;
+    }
+    else{
+      pre++;
+      if(fmt[pre]== 's'){
+        char *tmp = va_arg(Args,char*);
+        strcat(out,tmp);
+        return_val += strlen(tmp);
+      }
+      else if(fmt[pre] == 'd'){
+        int tmp_num = va_arg(Args,int);
+        char tmp[50]="";
+        zx_itoa(tmp_num,tmp,10);
+        strcat(out,tmp);
+        return_val += strlen(tmp);
+      }
+      else {
+        printf("to be continue\n");
+        assert(0);
+      }
+      pre++;
+    }
+  }
+  va_end(Args);
+  return return_val;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
