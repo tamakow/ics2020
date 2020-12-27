a52e0cd15782190cd4c535074a5086adefa20ac8
tracer-ics2020 <tracer@njuics.org>
2020-12-27 22:53:27 +0800

    >  run 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: b7a57fa6b67147e2ad3c707fef06aff6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  22:53:27 up  9:11,  1 user,  load average: 0.92, 0.78, 0.87

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -8,7 +8,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = (inl(RTC_ADDR)-start_time)/1000;
+  uptime->us = (inl(RTC_ADDR)-start_time)/2;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
