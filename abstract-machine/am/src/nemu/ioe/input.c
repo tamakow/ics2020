92fb1467177e8b5bdd9437f291d3dcc633c95232
tracer-ics2020 <tracer@njuics.org>
2020-12-28 17:24:47 +0800

    >  run 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: e9bfc680d78f4713b83dd23c20e227f2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  17:24:47 up  2:13,  1 user,  load average: 1.68, 1.36, 1.26

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -4,6 +4,13 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  int temp = inl(KBD_ADDR);
+  if(temp != KEYDOWN_MASK){
+    kbd->keycode = temp;
+    kbd->keydown = true;
+  }
+  else{
+    kbd->keycode = AM_KEY_NONE;
+    kbd->keydown = false;
+  }
 }
