459a7385aa15c29c587db3b38a7d44445ec66c8e
tracer-ics2020 <tracer@njuics.org>
2020-12-28 16:25:43 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: e9bfc680d78f4713b83dd23c20e227f2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  16:25:43 up  1:14,  1 user,  load average: 1.20, 1.16, 1.09

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -83,6 +83,11 @@ static inline def_EHelper(rol) {
   }
   operand_write(s,id_dest,s2);
   rtl_set_CF(s,s0);
+  if(*dsrc1 == 1){
+    rtl_shri(s,s1,s2,id_dest->width*8-1);
+    rtl_setrelop(s,RELOP_NE,s2,s0,s1);
+    rtl_set_OF(s,s2);
+  }
   print_asm_template2(rol);
 }
 
