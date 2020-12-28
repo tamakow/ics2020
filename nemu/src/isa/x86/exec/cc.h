5e1d4bfbc3cbfc9cb5398ac33f505c980b0a867b
tracer-ics2020 <tracer@njuics.org>
2020-12-28 16:09:37 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: e9bfc680d78f4713b83dd23c20e227f2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  16:09:37 up 58 min,  1 user,  load average: 0.81, 0.90, 0.96

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -53,7 +53,7 @@ static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcod
       rtl_or(s,dest,dest,t0);
       break;
     }
-       //TODO();
+    TODO();
     default: panic("should not reach here");
     case CC_P: panic("PF is not supported");
   }
