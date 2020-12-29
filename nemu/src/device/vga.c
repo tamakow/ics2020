37c66f27af5e69442feccc756f6378d43a620239
tracer-ics2020 <tracer@njuics.org>
2020-12-29 23:15:30 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: f4f310cd1f7743e18574f35e7f39b1e0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  23:15:31 up 49 min,  1 user,  load average: 0.34, 0.57, 0.57

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -40,6 +40,12 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  assert(0);
+  /*
+  if(){
+    update_screen();
+  }
+  */
 }
 
 void init_vga() {
