e6e54770bdda7b0b287acd46861cd2c9da0d0623
tracer-ics2020 <tracer@njuics.org>
2020-12-30 19:44:22 +0800

    >  run 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: f4f310cd1f7743e18574f35e7f39b1e0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  19:44:22 up 21:15,  1 user,  load average: 2.72, 2.30, 1.91

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-// #include <string.h>
+#include <string.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 // #define W 400
@@ -8,12 +8,14 @@
 static inline int min(int x, int y) { return (x < y) ? x : y; }
 
 void __am_gpu_init() {
+  /*
   int i;
   int w = 400 ;  // TODO: get the correct width
   int h = 300 ;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
+  */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -25,7 +27,6 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  /*
   uint32_t tmp = inl(VGACTL_ADDR);
   int W = tmp >> 16;
   int H = tmp & 0xffffffffffffffff;
@@ -37,7 +38,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
     pixels += w;
   }
-  */
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
