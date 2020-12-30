50c42febf053e37000e605ea94a7274246566a3e
tracer-ics2020 <tracer@njuics.org>
2020-12-30 17:36:55 +0800

    >  run 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: f4f310cd1f7743e18574f35e7f39b1e0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  17:36:55 up 19:07,  1 user,  load average: 1.21, 1.31, 1.20

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -9,8 +9,8 @@ static inline int min(int x, int y) { return (x < y) ? x : y; }
 
 void __am_gpu_init() {
   int i;
-  int w = 400;//io_read(AM_GPU_CONFIG).width ;  // TODO: get the correct width
-  int h = 300;//io_read(AM_GPU_CONFIG).height ;  // TODO: get the correct height
+  int w = io_read(AM_GPU_CONFIG).width ;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height ;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -25,7 +25,6 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  /*
   int W = io_read(AM_GPU_CONFIG).width;
   int H = io_read(AM_GPU_CONFIG).height;
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
@@ -36,7 +35,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
     pixels += w;
   }
-  */
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
