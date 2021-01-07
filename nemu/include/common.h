91f166d0cec9781b646eaadd9e203ca4e34b4c18
tracer-ics2020 <tracer@njuics.org>
2021-01-07 11:25:02 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 6b084e4a05074f27977f86b5a2decceb   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:25:03 up 15:56,  1 user,  load average: 1.03, 1.25, 1.20

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,9 +1,9 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define DEBUG
+//#define DEBUG
 //#define DIFF_TEST
-volatile static int dummy;
+
 
 #if _SHARE
 // do not enable these features while building a reference design
