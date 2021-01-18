54b3961f786e62b5eb92ca6dc27a0e1280b4aa81
tracer-ics2020 <tracer@njuics.org>
2021-01-18 12:31:11 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: edf3f6f2f13c42358df6602fc61cb8ea   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  12:31:11 up 1 day, 9 min,  1 user,  load average: 0.44, 0.37, 0.50

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -14,14 +14,15 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 
   uint32_t addr = cpu.idtr.base + NO*8;
 
-  uint32_t off_h = vaddr_read(addr+4,4);
-  uint32_t off_l = vaddr_read(addr,4);
+  uint32_t off_h = vaddr_read(addr+6,2);
+  uint32_t off_l = vaddr_read(addr,2);
   uint32_t p = vaddr_read(addr+5,1) >> 7;
-  printf("offset_h : 0x%x\n offset_l : 0x%x\n p : %d",off_h,off_l,p);
+  // printf("offset_h : 0x%x\n offset_l : 0x%x\n p : %d",off_h,off_l,p);
   if(!p) assert(0);
   
-  uint32_t offset = (off_h&0xffff0000) | (off_l&0xffff);
-  printf("0x%x",offset);
+  // uint32_t offset = (off_h&0xffff0000) | (off_l&0xffff);
+  uint32_t offset = (off_h << 16) + off_l;
+  // printf("0x%x",offset);
   rtl_j(s,offset);
 }
 
