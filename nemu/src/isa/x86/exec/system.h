9df485b494e39212559feeff0adef3e342b03f78
tracer-ics2020 <tracer@njuics.org>
2021-01-21 19:48:14 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: adc652bee1e641c2a6e07c25cbf82e41   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  19:48:14 up 28 min,  1 user,  load average: 0.38, 0.47, 0.52

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -11,8 +11,9 @@ static inline def_EHelper(lidt) {
   // TODO();
   // rtl_lm(s,s0,ddest,0,id_dest->width);
   rtl_li(s,s0,*ddest);
-  printf("addr = 0x%x\n s0 is 0x%x",*ddest,*s0);
+  printf("addr = 0x%x\n s0 is 0x%x\n",*ddest,*s0);
   cpu.idtr.limit=vaddr_read(*s0,2);
+  printf("limit is 0x%x\n",cpu.idtr.limit);
   rtl_li(s,&cpu.idtr.base, vaddr_read(*s0+2,4));
   printf("base is 0x%x\n",cpu.idtr.base);
   print_asm_template1(lidt);
