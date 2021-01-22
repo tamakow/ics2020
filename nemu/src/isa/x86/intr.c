75efdf4f0745539ba8866ce98abf651ad405b885
tracer-ics2020 <tracer@njuics.org>
2021-01-22 11:25:16 +0800

    >  compile 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 106112d9b0174647a57c942a88f2941e   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  11:25:16 up  1:19,  1 user,  load average: 0.38, 0.63, 0.72

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -21,7 +21,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   if(!p) assert(0);
   
   // uint32_t offset = (off_h&0xffff0000) | (off_l&0xffff);
-  uint32_t offset = (off_h << 16) + off_l;
+  uint32_t offset = (off_h << 16) | off_l;
   // printf("0x%x",offset);
   rtl_j(s,offset);
 }
