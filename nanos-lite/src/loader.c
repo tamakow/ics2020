b728507c8fd13c5349c3f4c3045e3e231c996337
tracer-ics2020 <tracer@njuics.org>
2021-02-06 21:47:01 +0800

    >  run 191240080    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: dec800d6bd6646c9becbb5f371870267   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-41-generic       Architecture: x86-64  21:47:01 up  3:54,  1 user,  load average: 0.70, 0.43, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,10 @@
 #include <proc.h>
 #include <elf.h>
 
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -10,8 +14,20 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // TODO();
+  Elf_Ehdr eh;
+  Elf_Phdr ph;
+
+  ramdisk_read(&eh,0,sizeof(Elf_Ehdr));
+
+  for(int i=0;i<eh.e_phnum;++i){
+    ramdisk_read(&ph,eh.e_phoff+i*sizeof(Elf_Phdr),sizeof(Elf_Phdr));
+    if(ph.p_type == 1){
+      ramdisk_read((void*)ph.p_vaddr,ph.p_offset,ph.p_filesz);
+      memset((void*)ph.p_vaddr + ph.p_filesz , 0, ph.p_memsz - ph.p_filesz);
+    }
+  }
+  return eh.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
